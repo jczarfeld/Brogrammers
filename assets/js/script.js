c9b0902aa02d7ecce58ec3ad978247f835d62cc6
John Osterfeld <jczarfeld@gmail.com>
2021-07-07 18:28:14 -0400

    Was able to streamline the code a bit, and set intial frame to take user input for muscle categroy. Also started the code logic for the exercise_base image search using the filter method.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,116 @@
-//var apiKey = 'd9eb4f1711b455af97554fa7e0564ee5fb06b502'
 
-//console.log("wger.de/en/exercise/muscle/overview/");
-
-var oneClickUrl = "https://wger.de/api/v2/exercise/?language=2&limit=229";
-        fetch(oneClickUrl)
-          .then(function (res) {
+// make one big function that contains both api calls to compare the exercise bases
 
 
-            return res.json();
+// make calls and figure out IDs for each body part
+
+var inputCategory = 10;
+
+// This takes the category number, inserts it into the url vairable and selects a random workout
+// Then this logs the exercise base from the workout chosen
+
+var categoriesUrl = "https://wger.de/api/v2/exercise/?category=" + inputCategory;
+        fetch(categoriesUrl)
+          .then(function (catRes) {
+
+
+            return catRes.json();
           })
-          .then(function (data) {
+          .then(function (catData) {
+           console.log(catData);
+           var catItem = catData.results[Math.floor(Math.random()*catData.results.length)];
+
+           console.log(catItem);
+           console.log(catItem.name);
+           console.log(catItem.description);
+           console.log(catItem.exercise_base);
+
+           
+
+           // still need to move this number into a comparison filter against the images json
+
+          
+
+          
+           var twoClickUrl = "https://wger.de/api/v2/exerciseimage";
+           fetch(twoClickUrl)
+             .then(function (res2) {
+   
+               //loop over the data got back and pull out all with same base(use filter-return brand new array)
+   
+               
+   
+   
+               return res2.json();
+             })
 
 
-            console.log(data);
-          })
+             .then(function (data2) {
+   
+   
+              console.log(data2);
+
+
+            // need to move this outside of the function? need local storage?
+
+              let matchingEBase = data2.results.filter( (obj) => {
+                if(obj.exercise_base== catItem.exercise_base) {
+                  console.log(matchingEBase.exercise_base);
+
+                }})
+              })
+
+            })
+
+                
+             
+   
+   
+               
+   
+             
+          
+          
+        
+
+
+
+
+//var oneClickUrl = "https://wger.de/api/v2/exercise/?language=2&limit=229";
+     //   fetch(oneClickUrl)
+      //    .then(function (res) {
+
+
+       //     return res.json();
+       //   })
+        //  .then(function (data) {
+
+
+            // the use input will give the data.results[] identifying number, then we can grab the exercise_base
+
+
+        //    console.log(data);
+         //   console.log(data.results[19].exercise_base);
+              //create secondary function to call with the base that comes back from muscle grp use this to get img
+          // getImages(data.results[19].exercise_base);
+
+
+           
+
+
+
+
+
+
+
+
+
+
+         // })
+
+
+
+
           
 
     
