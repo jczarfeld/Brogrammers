9af21dbefc0717a51a61e48aeda836fc8f7d76ab
John Osterfeld <jczarfeld@gmail.com>
2021-07-08 12:24:43 -0400

    updated js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,22 @@
 resultContentEl = document.querySelector("#result-content");
-// make one big function that contains both api calls to compare the exercise bases
-
-
-// make calls and figure out IDs for each body part
-
-var inputCategory = 8;
+var workoutBtn = document.getElementById("getWorkout");
 
 // This takes the category number, inserts it into the url vairable and selects a random workout
 // Then this logs the exercise base from the workout chosen
 
 
-function categorySelected() {
+function getMyWorkout(event) {
+  event.preventDefault();
+  var workoutChoice = document.querySelector('#workout-input').options[document.querySelector('#workout-input').selectedIndex].getAttribute("value");
+  var timeChoice = document.querySelector('#time-input')
+  console.log(workoutChoice);
+  console.log(timeChoice);
 
+  getExercises(workoutChoice, timeChoice)
+}
+
+
+    function getExercises(workoutChoice, timeChoice) {
   var categoriesUrl = "https://wger.de/api/v2/exercise/?category=" + inputCategory;
   fetch(categoriesUrl)
     .then(function (catRes) {
@@ -67,104 +72,19 @@ function printResults(catItem) {
 }
 
 
+workoutBtn.addEventListener("click", getMyWorkout);
 
 
-// still need to move this number into a comparison filter against the images json
 
 
 
 
-//var twoClickUrl = "https://wger.de/api/v2/exerciseimage/?limit=118";
-//fetch(twoClickUrl)
-//  .then(function (res2) {
 
-//loop over the data got back and pull out all with same base(use filter-return brand new array)
 
 
 
 
-//  return res2.json();
-// })
 
 
-// .then(function (data2) {
 
 
-//  console.log(data2);
-
-
-// getMatchingEBase(data2, catItem);
-
-//   let matchingEBase = data2.results.filter( (obj) => {
-//   if(obj.exercise_base == catItem.exercise_base) {
-//    console.log(matchingEBase);
-//   }})
-
-
-// need to move this outside of the function? need local storage?
-
-
-
-
-// function getMatchingEBase(data2, catItem) {
-
-
-//let matchingEBase = data2.results.filter( (obj) => {
-//  if(obj.exercise_base == catItem.exercise_base) {
-//  console.log(matchingEBase);
-
-// }})}
-
-categorySelected();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//var oneClickUrl = "https://wger.de/api/v2/exercise/?language=2&limit=229";
-//   fetch(oneClickUrl)
-//    .then(function (res) {
-
-
-//     return res.json();
-//   })
-//  .then(function (data) {
-
-
-// the use input will give the data.results[] identifying number, then we can grab the exercise_base
-
-
-//    console.log(data);
-//   console.log(data.results[19].exercise_base);
-//create secondary function to call with the base that comes back from muscle grp use this to get img
-// getImages(data.results[19].exercise_base);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// })
