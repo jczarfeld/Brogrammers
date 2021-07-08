e7f8662a4a650641d6c0a48b93c6128d0c22696d
mskaff <mskaff904@gmail.com>
2021-07-08 11:27:49 -0400

    updated JS to add even listener to submit button and then fire off getWorkout function and get exercised function

--- js/script.js
+++ js/script.js
@@ -1,12 +1,78 @@
-apiKey = "f85e16e4712c719f52ce489e766fd57443931aaf"
+// make one big function that contains both api calls to compare the exercise bases
+var workoutBtn = document.getElementById("getWorkout");
 
-    var queryUrl = "https://wger.de/en/software/api/v2/?format=json";
 
-    fetch(queryUrl)
-        .then(function (response) {
-            return response.json()
+// make calls and figure out IDs for each body part
+
+// This takes the category number, inserts it into the url vairable and selects a random workout
+// Then this logs the exercise base from the workout chosen
+function getMyWorkout(event) {
+    event.preventDefault();
+
+    var workoutChoice = document.querySelector('#workout-input').options[document.querySelector('#workout-input').selectedIndex].getAttribute("value");
+    var timeChoice = document.querySelector('#time-input')
+    console.log(workoutChoice);
+    console.log(timeChoice);
+
+    getExercises(workoutChoice, timeChoice)
+}
+
+function getExercises(workoutChoice, timeChoice) {
+
+    var categoriesUrl = "https://wger.de/api/v2/exercise/?category=" + workoutChoice + "&language=2";
+
+    fetch(categoriesUrl)
+        .then(function (catRes) {
+            return catRes.json();
         })
-        .then (function (data) {
-            console.log(data);
-        })
+        .then(function (catData) {
+            console.log(catData);
+            var catItem = catData.results[Math.floor(Math.random() * catData.results.length)];
 
+            console.log(catItem);
+            console.log(catItem.name);
+            console.log(catItem.description);
+            console.log(catItem.exercise_base);
+        
+    })
+
+}
+//        // still need to move this number into a comparison filter against the images json
+
+
+
+
+//        var twoClickUrl = "https://wger.de/api/v2/exerciseimage";
+//        fetch(twoClickUrl)
+//          .then(function (res2) {
+
+//            //loop over the data got back and pull out all with same base(use filter-return brand new array)
+
+
+
+
+//            return res2.json();
+//          })
+
+
+//          .then(function (data2) {
+
+
+//           console.log(data2);
+
+
+//         // need to move this outside of the function? need local storage?
+
+//           let matchingEBase = data2.results.filter( (obj) => {
+//             if(obj.exercise_base== catItem.exercise_base) {
+//               console.log(matchingEBase.exercise_base);
+
+//             }})
+//           })
+
+//         })
+
+
+
+
+workoutBtn.addEventListener("click", getMyWorkout);
