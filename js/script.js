c9d21c007429226c93082ab366810229c14a54d6
mskaff <mskaff904@gmail.com>
2021-07-08 12:17:52 -0400

    added printResults function to JS

--- js/script.js
+++ js/script.js
@@ -36,7 +36,29 @@ function getExercises(workoutChoice, timeChoice) {
         
     })
 
-}
+};
+
+function printResults(catItem) {
+    console.log(catItem);
+    var resultCard = document.createElement('div');
+    resultCard.classList.add('card', 'mb-3', 'p-3');
+    var resultBody = document.createElement('div');
+    resultBody.classList.add('card-body');
+    resultCard.append(resultBody);
+    var nameContentEl = document.createElement('p');
+    nameContentEl.innerHTML =
+      '<strong>Exercise Name: </strong> ' + catItem.name + '<br/>';
+    var descriptionContentEl = document.createElement('p');
+    descriptionContentEl.innerHTML =
+      '<strong>Exercise Description: </strong> ' + catItem.description + '<br/>';
+    var linkButtonEl = document.createElement('a');
+    linkButtonEl.textContent = 'Read More';
+    linkButtonEl.setAttribute('href', "https://wger.de/en/exercise/307/view/" + catItem.name.text);
+    linkButtonEl.classList.add('btn', 'btn-dark');
+    resultBody.append(nameContentEl, descriptionContentEl, linkButtonEl);
+    resultContentEl.append(resultCard);
+  }
+
 //        // still need to move this number into a comparison filter against the images json
 
 
