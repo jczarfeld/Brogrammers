2889435fe088b6b054218522295d59d660b94416
John Osterfeld <jczarfeld@gmail.com>
2021-07-09 12:38:55 -0400

    got weather call to work and print to html2

--- js/script.js
+++ js/script.js
@@ -1,50 +1,55 @@
 // make one big function that contains both api calls to compare the exercise bases
 var workoutBtn = document.getElementById("getWorkout");
+resultContentEl = document.getElementById("result-content");
 // make calls and figure out IDs for each body part
 // This takes the category number, inserts it into the url vairable and selects a random workout
 // Then this logs the exercise base from the workout chosen
 function getMyWorkout(event) {
-  event.preventDefault();
+    event.preventDefault();
+    var workoutChoice = document.querySelector('#workout-input').options[document.querySelector('#workout-input').selectedIndex].getAttribute("value");
+    var timeChoice = document.querySelector('#time-input').options[document.querySelector('#time-input').selectedIndex].getAttribute("value");
+    var cityChoice = document.querySelector('#location').value;
+    
+    console.log(cityChoice);
+    localStorage.setItem("city", cityChoice);
+    console.log(workoutChoice);
+    console.log(timeChoice);
+    
 
-  var workoutChoice = document.querySelector('#workout-input').options[document.querySelector('#workout-input').selectedIndex].getAttribute("value");
-  var timeChoice = document.querySelector('#time-input').options[document.querySelector('#time-input').selectedIndex].getAttribute("value");
-  console.log(workoutChoice);
-  console.log(timeChoice);
 
-  location.href = "./index2.html"
-
-  getExercises(workoutChoice, timeChoice)
+    getExercises(workoutChoice, timeChoice)
 }
-
 function getExercises(workoutChoice, timeChoice) {
-  var categoriesUrl = "https://wger.de/api/v2/exercise/?category=" + workoutChoice + "&language=2";
-  fetch(categoriesUrl)
-    .then(function (catRes) {
-      return catRes.json();
-    })
-    .then(function (catData) {
-      console.log(catData);
+    var categoriesUrl = "https://wger.de/api/v2/exercise/?category=" + workoutChoice + "&language=2";
+    fetch(categoriesUrl)
+        .then(function (catRes) {
+            return catRes.json();
+        })
+        .then(function (catData) {
+            console.log(catData);
 
-      // for (var i = 0; i < timeChoice, i++) {
+                var finalWorkout = [];
 
-      var catItem = catData.results[Math.floor(Math.random() * catData.results.length)];
+            for (var i = 0; i < timeChoice; i++) {
 
+                var catItem = catData.results[Math.floor(Math.random() * catData.results.length)];
 
-      console.log(catItem);
-      console.log(catItem.name);
-      console.log(catItem.description);
-      console.log(catItem.exercise_base);
+                finalWorkout.push(catItem);
 
-      //this tuurns the object into a string so it can be stored and moved
-      var storedWorkout = JSON.stringify(catItem);
-      console.log(storedWorkout);
-      localStorage.setItem("storedWorkout", storedWorkout);
+                console.log(finalWorkout);
 
-      // }
-    })
-
-  // printResults(catItem);
-
-}
-
+                
+                console.log(catItem);
+                console.log(catItem.name);
+                console.log(catItem.description);
+                console.log(catItem.exercise_base);
+                var storedWorkout = JSON.stringify(finalWorkout);
+                console.log(storedWorkout);
+                localStorage.setItem("storedWorkout", storedWorkout);
+                
+                location.href = "./index2.html"
+            }
+        })
+            // printResults(catItem);
+    }
 workoutBtn.addEventListener("click", getMyWorkout);
--- js/script2.js
+++ js/script2.js
@@ -1,7 +1,10 @@
 resultContentEl = document.querySelector("#result-content");
 var newWorkout = localStorage.getItem("storedWorkout");
+var getCity = localStorage.getItem("city");
+var apiKey = "9da4bb573ba9574c53ffbc53979faaa4";
 
 console.log(newWorkout);
+console.log(getCity);
 
 //this turns the data back into an accessible object
 
@@ -10,6 +13,12 @@ console.log(resultsWorkout);
 console.log(resultsWorkout.name);
 
 //this put the name, description and link button onto the second html
+
+for (let index = 0; index < resultsWorkout.length; index++) {
+    if (resultsWorkout[index].name !== undefined) {
+        printResults(resultsWorkout[index])
+    }
+}
 
 function printResults(resultsWorkout) {
     console.log(resultsWorkout.name);
@@ -34,4 +43,64 @@ function printResults(resultsWorkout) {
     resultBody.append(nameContentEl, descriptionContentEl, linkButtonEl);
     resultContentEl.append(resultCard);
 }
-printResults(resultsWorkout);
+function forecast (getCity) {
+    var forecastHeader = document.getElementById("forecast-header");
+    forecastHeader.removeAttribute("class", "invisible");
+
+    var forecastDisplay = document.getElementById("forecast-display");
+    forecastDisplay.textContent = "";
+
+    var forecastApi = "https://api.openweathermap.org/data/2.5/forecast?q=" + getCity + "&appid=" + apiKey + "&units=imperial";
+
+    console.log(forecastApi);
+
+    // fetch forecast information
+    fetch(forecastApi)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function(data) {
+            console.log(data);
+            
+            // iterate over data and only pull back time with "12:00" present (returns several times for each day)
+           // for (var i = 0; i < data.list.length; i++) {
+            //    if (data.list[i].dt_txt.includes("12:00")) {
+                
+                var forecastEl = document.getElementById("forecast-display");
+                var forecastCards = document.createElement('div');
+                var forecastContainer = document.createElement('div');
+                var forecastCard = document.createElement('div');
+                var forecastHeader = document.createElement('h3');
+                var forecastWind = document.createElement('p');
+                var forecastHumidity = document.createElement('p');
+                var forecastTemperature = document.createElement('p');
+
+                forecastContainer.className = "card bg-primary text-white justify-content-evenly";
+
+                forecastCard.className = "card-body p-2 me-1";
+
+                forecastHeader.className = "card-title";
+              //  forecastHeader.textContent = moment(data.list[i].dt_txt.split("12:")[0]).format("MMMM Do, YYYY");
+                
+                forecastWind.className = "card-text";
+                forecastWind.textContent = "Wind Speed: " + data.list[0].wind.speed + " MPH";
+                forecastHumidity.className = "card-text"
+                forecastHumidity.textContent = "Humidity : " + data.list[0].main.humidity + "%";
+                forecastTemperature.className = "card-text"
+                forecastTemperature.textContent = "Temperature : " + data.list[0].main.temp_max + "°F";
+
+
+
+                forecastCard.appendChild(forecastHeader);
+                forecastCard.appendChild(forecastWind);
+                forecastCard.appendChild(forecastHumidity);
+                forecastCard.appendChild(forecastTemperature);
+                forecastContainer.appendChild(forecastCard);
+                forecastCards.appendChild(forecastContainer);
+                forecastEl.appendChild(forecastContainer);
+                }   
+        )}        
+        
+
+
+forecast(getCity);
